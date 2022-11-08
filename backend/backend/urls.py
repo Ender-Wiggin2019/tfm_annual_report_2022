14a440e2341adcbcf97ae39ae01bbd460d8e8682
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2022-11-08 20:34:00 +0000

    add host

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_fr Famework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/account/', include('account.urls')),
